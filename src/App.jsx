25ed47694b72dbb0548eeafd63bb70bf55d21531
tonyishangu <tonyishangu@gmail.com>
2026-03-17 13:24:35 +0300

    setup the app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Sidebar from './components/layout/Sidebar'
+import Navbar from './components/layout/Navbar'
+import AppRoutes from './routes/AppRoutes'
+
 
 function App() {
   
   return (
-    <>
-      
-    </>
+    <div className='flex h-screen bg-gray-100 dark:bg-gray-900'>
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <main className="p-6 overflow-y-auto">
+          <AppRoutes />
+        </main>
+      </div>
+    </div>
   )
 }
 
