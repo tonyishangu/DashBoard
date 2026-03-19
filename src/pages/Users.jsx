570be99ae7b54003188350a933e92a806e5a9686
tonyishangu <tonyishangu@gmail.com>
2026-03-19 11:29:25 +0300

    created the users component to show the users

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Table from '../components/ui/Table'
 
 const Users = () => {
+  const users = [
+    {name: 'Tony', email: 'tony@gmail.com', role: 'Admin'},
+    {name: 'Brian', email: 'brian@gmail.com', role: 'Editor'}
+  ]
   return (
-    <div>Users</div>
+    <div>
+      <h1 className='text-2xl font-bold mb-6'>
+        Users
+      </h1>
+      <Table data={users} />
+
+    </div>
   )
 }
 
