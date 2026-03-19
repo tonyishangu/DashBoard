44d1cebad86fc00a7f2a69b6ecc5b966b7f62257
tonyishangu <tonyishangu@gmail.com>
2026-03-19 11:04:05 +0300

    created a table component

--- src/components/ui/Table.jsx
+++ src/components/ui/Table.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Table = () => {
+const Table = ({data}) => {
   return (
-    <div>Table</div>
+    <table className='w-full bg-white shadow rounded'>
+      <thead className='bg-gray-100'>
+        <tr>
+          <th className='p-3'>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((user, index) => (
+          <tr key={index} className='border-t'>
+            <td clasName='p-3'>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.role}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
